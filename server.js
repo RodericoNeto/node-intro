148df036427bc5f54a8a4df482b5b0c58417c9ca
RodericoNeto <talysonsoares40@gmail.com>
2022-10-11 20:58:59 -0300

    node server

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const http = require('http')
 
 const hostname = '127.0.0.1'
-const port = 3000
+const port = 5000
 
 http.createServer((req, res) => {
+    console.log(req.url)
+    if(req.url === '/'){
+        res.end('Home')
+    }
+
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain; charset=utf-8');
     res.end('Olá, rod!\n');
